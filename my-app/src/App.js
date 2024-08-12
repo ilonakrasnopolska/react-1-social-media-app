1b0f67d3a2049d412b9730d450df483dcc7001df
Ilona Krasnopolski <ilonakrasnopolskaya8@gmail.com>
2024-08-12 19:57:07 +0300

    initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from "./modules/header";
+import Technologies from "./modules/technologies";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Technologies />
+    </div>
+  );
+}
+
+export default App;
