91abb6e71cffe4905993971f20c43342c14c1a8c
Ilona Krasnopolski <ilonakrasnopolskaya8@gmail.com>
2024-08-17 05:43:05 +0300

    added icons

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
-import Header from "./modules/Header";
-import Sidebar from "./modules/Sidebar";
-import Footer from "./modules/Footer";
-import Main from "./modules/Main";
+import Header from "./modules/components/Header";
+import Sidebar from "./modules/components/Sidebar";
+import Footer from "./modules/components/Footer";
+import Profile from "./modules/components/Profile";
 
 const App = () => {
   return (
     <div className="app-container">
       <Header />
       <Sidebar />
-      <Main />
+      <Profile />
       <Footer />
     </div>
   );
--- src/modules/components/Footer.jsx
+++ src/modules/components/Footer.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import {Instagram, Facebook, Telegram} from "../helpers/SVG-icons";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      social media app.com
+      <div className="footer__container">
+        <ul className="footer__icons">
+          <li className="footer__icon">
+            <a className="footer__link" href="#">
+              <Instagram />
+            </a>
+          </li>
+          <li className="footer__icon">
+            <a className="footer__link" href="#">
+              <Facebook/>
+            </a>
+          </li>
+          <li className="footer__icon">
+            <a className="footer__link" href="#">
+              <Telegram/>
+            </a>
+          </li>
+        </ul>
+        <div className="footer__text">
+          Copyright @ 2024 Chakra
+        </div>
+      </div>
     </footer>
   );
 }
